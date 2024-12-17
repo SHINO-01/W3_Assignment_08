d086a329ad5c6b1c35fbd677363457d5d5108b52
SHINO-01 <sakifhussain33@gmail.com>
2024-12-17 12:02:57 +0600

    minor update

--- hotel_scrapper/pipelines.py
+++ hotel_scrapper/pipelines.py
@@ -1,5 +1,6 @@
 import sqlalchemy
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
 
 Base = declarative_base()
@@ -25,14 +26,15 @@ class HotelScrapperPipeline:
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
-    def process_item(self, item, spider=None):
+    def process_item(self, item, spider):
         session = self.Session()
         try:
             hotel = Hotel(**item)
             session.add(hotel)
             session.commit()
+            spider.log(f"Successfully added to database: {item}")
         except Exception as e:
-            print(f"Error adding to database: {e}")
+            spider.log(f"Error adding to database: {e}")
             session.rollback()
         finally:
             session.close()
